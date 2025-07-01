3193b72505c4281c2354e138752ed0d577e63473
neeraj015 <neerajmaurya015@gmail.com>
2025-07-02 04:01:38 +0530

    setup and require.. added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    #this function will return the list of requirements
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace('\n',"") for req in requirements]
+    
+    return requirements
+
+
+setup(
+name='DSproject',
+version='0.0.1',
+author='Neeraj',
+author_email='neerajmaurya015@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
