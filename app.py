14681bbe415ac242af82c00e6b45e4985a7dd16b
neeraj015 <neerajmaurya015@gmail.com>
2025-07-06 21:15:17 +0530

    after adding ingestion code

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__== "__main__":
@@ -7,7 +9,11 @@ if __name__== "__main__":
 
 
 try:
-    a=1/0
+    data_ingestion_config=DataIngestionConfig()
+    data_ingestion=DataIngestion()
+    data_ingestion.initiate_data_ingestion()
+
+
 except Exception as e:
     logging.info("Custom Exception")
     raise CustomException(e,sys)
